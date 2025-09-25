1bd2cfe3ee312d04630b228b814bd1959984ef72
tizu69 <60812901+tizu69@users.noreply.github.com>
2025-09-25 18:00:43 +0200

    home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { store } from '$lib/monystore';
+	import { page } from '$app/state';
 
 	let { children } = $props();
 </script>
@@ -8,5 +10,19 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
+<header class="flex space-x-2 overflow-x-scroll p-4">
+	{#snippet card(text: string, href: string)}
+		{@const current = page.url.pathname === new URL(href, page.url).pathname}
+		<a {href} class={['rounded-full px-2', current ? 'bg-accent text-background' : 'bg-layer']}>
+			{text}
+		</a>
+	{/snippet}
+
+	{#each store.current.projects as project}
+		{@render card(project.name, `/p/${project.id}`)}
+	{/each}
+	{@render card('+', `/`)}
+</header>
 
 {@render children?.()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { store } from '$lib/monystore';
+</script>
+
+<main class="flex min-h-[80dvh] flex-col items-center justify-center p-4">
+	<h1 class="mb-4 text-4xl font-bold">mony</h1>
+
+	<form
+		onsubmit={(e) => {
+			e.preventDefault();
+			const formData = new FormData(e.currentTarget);
+
+			const name = formData.get('name')?.toString().trim();
+			if (!name) return;
+
+			const project = store.addProject(name);
+			goto(`/p/${project.id}`);
+		}}
+	>
+		<input
+			type="text"
+			placeholder="Project name"
+			required
+			name="name"
+			class="mr-2 rounded bg-layer p-2"
+		/>
+		<button type="submit" class="rounded bg-accent px-4 py-2 font-bold text-background">
+			-&gt;
+		</button>
+	</form>
+</main>
