e8b22093d4866f21fa3ee17380caaac91a53b77b
tizu69 <60812901+tizu69@users.noreply.github.com>
2025-09-28 09:13:11 +0200

    hide global stats when no transactions ever made

--- src/routes/StatGuesser.svelte
+++ src/routes/StatGuesser.svelte
@@ -11,42 +11,44 @@
 	let totalProj = $derived(projects.length);
 </script>
 
-<div class="space-y-4 px-4">
-	<div>
-		<div class="flex flex-col rounded-lg bg-layer p-4">
-			<p class="text-sm text-subtext">Total added</p>
-			<Number small value={totalIn} />
+{#if totalPurch > 0}
+	<div class="space-y-4 px-4">
+		<div>
+			<div class="flex flex-col rounded-lg bg-layer p-4">
+				<p class="text-sm text-subtext">Total added</p>
+				<Number small value={totalIn} />
+			</div>
+		</div>
+		<div>
+			<div class="flex flex-col rounded-lg bg-layer p-4">
+				<p class="text-sm text-subtext">Total spent</p>
+				<Number small value={-totalOut} />
+			</div>
+		</div>
+		<div class="grid grid-cols-[1fr_auto] space-x-4">
+			<div class="flex flex-col rounded-lg bg-layer p-4">
+				<p class="text-sm text-subtext">Left over</p>
+				<Number small value={totalAvail} />
+			</div>
+			<div class="flex flex-col rounded-lg bg-layer p-4">
+				<p class="text-sm text-subtext">Saved</p>
+				<Number
+					small
+					value={Math.round(savingRate !== savingRate ? 0 : savingRate)}
+					type="number"
+					suffix="%"
+				/>
+			</div>
+		</div>
+		<div class="grid grid-cols-2 space-x-4">
+			<div class="flex flex-col rounded-lg bg-layer p-4">
+				<p class="text-sm text-subtext">Purchases</p>
+				<Number small value={totalPurch} type="number" />
+			</div>
+			<div class="flex flex-col rounded-lg bg-layer p-4">
+				<p class="text-sm text-subtext">Projects</p>
+				<Number small value={totalProj} type="number" />
+			</div>
 		</div>
 	</div>
-	<div>
-		<div class="flex flex-col rounded-lg bg-layer p-4">
-			<p class="text-sm text-subtext">Total spent</p>
-			<Number small value={-totalOut} />
-		</div>
-	</div>
-	<div class="grid grid-cols-[1fr_auto] space-x-4">
-		<div class="flex flex-col rounded-lg bg-layer p-4">
-			<p class="text-sm text-subtext">Left over</p>
-			<Number small value={totalAvail} />
-		</div>
-		<div class="flex flex-col rounded-lg bg-layer p-4">
-			<p class="text-sm text-subtext">Saved</p>
-			<Number
-				small
-				value={Math.round(savingRate !== savingRate ? 0 : savingRate)}
-				type="number"
-				suffix="%"
-			/>
-		</div>
-	</div>
-	<div class="grid grid-cols-2 space-x-4">
-		<div class="flex flex-col rounded-lg bg-layer p-4">
-			<p class="text-sm text-subtext">Purchases</p>
-			<Number small value={totalPurch} type="number" />
-		</div>
-		<div class="flex flex-col rounded-lg bg-layer p-4">
-			<p class="text-sm text-subtext">Projects</p>
-			<Number small value={totalProj} type="number" />
-		</div>
-	</div>
-</div>
+{/if}
